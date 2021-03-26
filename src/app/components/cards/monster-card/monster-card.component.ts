781d8c3951d5d482f4a182b13e42c7c2494cc2f5
Xavier Huberdeau <xavier.huberdeau@thetribe.io>
2021-03-26 18:52:44 +0100

    Style monster card

--- src/app/components/cards/monster-card/monster-card.component.ts
+++ src/app/components/cards/monster-card/monster-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IMonsterCard } from '../../../game-classes/game-types.model';
 
 @Component({
   selector: 'app-monster-card',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./monster-card.component.scss']
 })
 export class MonsterCardComponent implements OnInit {
-
+  @Input() monster: IMonsterCard;
+  monsterUrl: string;
   constructor() { }
 
   ngOnInit(): void {
+    this.monsterUrl = `https://www.robohash.org/${Math.random().toString(36).substring(7)}?set=set2`;
   }
 
 }
